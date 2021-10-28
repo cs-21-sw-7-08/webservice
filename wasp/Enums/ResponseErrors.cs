ab2806e561059b9428622913d86907732e3398b3
ProgDaniel <43956461+ProgDaniel@users.noreply.github.com>
2021-10-28 12:05:28 +0200

    ResponseErrors enum ordered and categorized

--- wasp/Enums/ResponseErrors.cs
+++ wasp/Enums/ResponseErrors.cs
@@ -7,18 +7,29 @@ namespace WASP.Enums
 {
     public enum ResponseErrors
     {
+        /*Response codes for categorization
+         * 0-99 - System Errors
+         * 100-199 Issue-related Errors
+         * 200-299 Citizen-related Errors
+         * 300-399 Municipality-related errors
+         */
+
+        //System errors
         AnExceptionOccurredInAController = 1,
         AnExceptionOccurredInTheDAL = 2,
-
+        //Issue-related errors
         CategoryDoesNotExist = 100,
         SubCategoryDoesNotExist = 101,
-        MunicipalityDoesNotExist = 102,
-        PleaseProvideBothToAndFromTime = 103,
-        IssueStateDoesNotExist = 104,
-        CitizenDoesNotExist = 105,
-        MunicipalityUserDoesNotExistsueDoesNotExist = 106,
-        MunicipalityUserEmailAndOrPasswordNotMatched = 200,
+        PleaseProvideBothToAndFromTime = 102,
+        IssueStateDoesNotExist = 103,
+        IssueDoesNotExist = 104,
+        //Citizen-related errors
+        CitizenDoesNotExist = 200,
         CitizenLoginBothEmailAndPhoneNumberCannotBeFilled = 201,
         CitizenWithTheseCredentialsHasNotBeenSignedUp = 202,
+        //Municipality-related errors
+        MunicipalityDoesNotExist = 300,
+        MunicipalityUserDoesNotExist = 301,
+        MunicipalityUserEmailAndOrPasswordNotMatched = 302,
     }
 }
